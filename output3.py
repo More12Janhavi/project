f0fb4703a9c1078ef5cbf08a4b8ddd6bb10904cf
More12Janhavi <168166302+More12Janhavi@users.noreply.github.com>
2024-04-26 13:27:01 +0530

    Add files via upload

---
+++ output3.py
@@ -0,0 +1,16 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: D:/my follder/program3.py
+Enter the value123
+Enter the value258
+
+ value for X= 123
+
+ value for Y= 258
+
+ Type of x= <class 'str'>
+
+ Type of y= <class 'str'>
+
+ Value of value 123258
